d20d26ee85b1c7ecf2d89ff260a52b6651cbebce
jitesh523 <jiteshjiju50@gmail.com>
2026-02-18 23:02:00 +0530

    feat: add TransformerModel, gradient boosting, StackingEnsemble to models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,4 +3,23 @@ from .baseline_model import BaselineModel
 from .lstm_model import LSTMModel
 from .anomaly_detector import AnomalyDetector
 
-__all__ = ['BaselineModel', 'LSTMModel', 'AnomalyDetector']
+try:
+    from .transformer_model import TransformerModel
+except ImportError:
+    TransformerModel = None
+
+try:
+    from .gradient_boosting_models import XGBoostRUL, LightGBMRUL
+except ImportError:
+    XGBoostRUL = None
+    LightGBMRUL = None
+
+try:
+    from .stacking_ensemble import StackingEnsemble
+except ImportError:
+    StackingEnsemble = None
+
+__all__ = [
+    'BaselineModel', 'LSTMModel', 'AnomalyDetector',
+    'TransformerModel', 'XGBoostRUL', 'LightGBMRUL', 'StackingEnsemble',
+]
